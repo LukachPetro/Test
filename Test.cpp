1c2841753585b90b01ef3a0e9fdd329796aaaf88
PL8693@gmail.com <322Lukach40887715>
2015-07-17 11:34:47 +0300

    2

--- Test.cpp
+++ Test.cpp
@@ -1,18 +1,4 @@
-// Test.cpp : Defines the entry point for the console application.
-//
-/*
 #include "stdafx.h"
-
-
-int _tmain(int argc, _TCHAR* argv[])
-{
-	return 0;
-}
-
-*/
-
-#include "stdafx.h"
-
 #include<iostream>
 #include<fstream>
 #include<stdio.h>
